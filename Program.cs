e59d0ed6891008f8d1249e30306b6928c62f7688
Edan Saedi <edansaedi@outlook.com>
2022-09-23 19:08:52 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using NeuralNetwork;
+
+namespace XOR_Neural_Network
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
