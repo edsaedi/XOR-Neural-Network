ed8cc2e6d75a267a0a5ceab592e593998235cae0
Edan Saedi <edansaedi@outlook.com>
2022-09-30 19:01:11 -0700

    Dendrites are null. Fix this issue.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,46 @@ namespace XOR_Neural_Network
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double[][] inputs = new double[][]
+            {
+                new double[]{0,0},
+                new double[]{1,0},
+                new double[]{0,1},
+                new double[]{1,1}
+            };
+            double[][] outputs = new double[][]
+            {
+                new double[]{0},
+                new double[]{1},
+                new double[]{1},
+                new double[]{0}
+            };
+
+            Random random = new Random();
+
+            NeuralNetwork.NeuralNetwork neuralNetwork = new NeuralNetwork.NeuralNetwork(ActivationAndErrorFunctions.TanHActivationFunction, ActivationAndErrorFunctions.MeanSqauredErrorFunction, 2, 2, 1);
+            neuralNetwork.Randomize(random, -1, 1);
+            while (true)
+            {
+                Console.SetCursorPosition(0, 0);
+                for (int i = 0; i < inputs.Length; i++)
+                {
+                    Console.Write("Inputs: ");
+                    for (int j = 0; j < inputs[i].Length; j++)
+                    {
+                        if (j != 0)
+                        {
+                            Console.Write(", ");
+                        }
+                        Console.Write(inputs[i][j]);
+                    }
+
+                    Console.Write(" Output: " + Math.Round(neuralNetwork.Compute(inputs[i])[0], 3));
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("Hello, World!");
+            }
         }
     }
 }
