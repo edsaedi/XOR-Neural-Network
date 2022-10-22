5be1027120f5c6bd1dfa33c19bfe16560506ee80
Edan Saedi <edansaedi@outlook.com>
2022-10-21 19:12:22 -0700

    The compute function is off because the outputs are always the same for each of the four cases. Check this for next time.

--- Program.cs
+++ Program.cs
@@ -35,8 +35,23 @@ namespace XOR_Neural_Network
                 population[i].Randomize(random, -1, 1);
             }
 
+            RunTraining(population, random, 2, inputs);
 
 
+            //for (int j = 0; j < 5; j++)
+            //{
+            //    double[] testers = new double[4];
+            //    Console.WriteLine("Here are the outputs: ");
+
+            //    for (int k = 0; k < 4; k++)
+            //    {
+            //        var temp = random.NextDouble();
+            //        testers[k] = temp;
+            //        Console.Write(temp + ", ");
+            //    }
+
+            //    Console.WriteLine("Fitness: " + Fitness(testers));
+            //}
             /*while (true)
             {
                 Console.SetCursorPosition(0, 0);
@@ -58,23 +73,70 @@ namespace XOR_Neural_Network
             }*/
         }
 
-        public static double Fitness(NeuralNetwork.NeuralNetwork network, double[][] inputs, double[] outputs)
+        public static double Fitness(double[][] outputs)
         {
             double fitness = 0;
-            for (int i = 0; i < inputs.Length; i++)
-            {
-                fitnses = 
-            }
 
-            //FINISH the fitness function.
-            return fitness;
+            //For output 1
+            fitness += Math.Pow((outputs[0][0]), 2);
+
+            //For output 2
+            fitness += Math.Pow(1 - outputs[1][0], 2);
+
+            //For output 3
+            fitness += Math.Pow(1 - outputs[2][0], 2);
+
+            //For output 4
+            fitness += Math.Pow(outputs[3][0], 2);
+
+            return fitness / 4;
         }
 
-        public static void RunTraining(NeuralNetwork.NeuralNetwork[] population, Random random, )
+        //public static double Fitness(NeuralNetwork.NeuralNetwork network, double[][] inputs)
+        //{
+        //    double fitness = 0;
+
+        //    for (int i = 0; i < inputs.Length; i++)
+        //    {
+        //        double[] outputs = network.Compute(inputs[i]);
+        //        //For output 1
+        //        fitness += (outputs[0]);
+
+        //        fitness += (network.Compute(inputs[i]))[0];
+
+        //        //For output 2
+        //        fitness += Math.Abs(1 - outputs[1]);
+
+        //        //For output 3
+        //        fitness += Math.Abs(1 - outputs[2]);
+
+        //        //For output 4
+        //        fitness += outputs[3];
+        //    }
+
+        //    return fitness;
+        //}
+
+        public static void RunTraining(NeuralNetwork.NeuralNetwork[] population, Random random, double mutationRate, double[][] inputs)
         {
             (NeuralNetwork.NeuralNetwork network, double fitness)[] fitnessRecords = new (NeuralNetwork.NeuralNetwork network, double fitness)[population.Length];
 
+            for (int i = 0; i < population.Length; i++)
+            {
+                fitnessRecords[i].network = population[i];
+                Console.WriteLine("Outputs: ");
+                var outputs = fitnessRecords[i].network.Compute(inputs);
 
+                for (int j = 0; j < outputs.Length; j++)
+                {
+                    Console.WriteLine(outputs[j][0] + ", ");
+                }
+
+                var fitnesS = Fitness(outputs);
+
+                fitnessRecords[i].fitness = fitnesS;
+                Console.WriteLine("Fitness: " + fitnesS + "\n");
+            }
         }
     }
 }
